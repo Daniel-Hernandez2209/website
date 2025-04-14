cb18b2501477c81f343778cccfc205aa12970e91
Daniel_HR181 <danielakire07@gmail.com>
2025-04-14 18:15:04 -0500

    modifique los archivos de html,css,js para la implementacion de la calculadora

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-alert('hola bienvenido a mi sitio web');
+const pantalla = document.getElementById('pantalla');
+
+    function agregar(valor) {
+      pantalla.value += valor;
+    }
+
+    function calcular() {
+      try {
+        pantalla.value = eval(pantalla.value);
+      } catch (e) {
+        pantalla.value = 'Error';
+      }
+    }
+
+    function limpiar() {
+      pantalla.value = '';
+    }
